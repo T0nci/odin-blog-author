336913f731b5c46216208b6dace273df62519bc5
T0nci <138928016+T0nci@users.noreply.github.com>
2024-12-12 19:51:19 +0100

    Install and setup React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router";
+import routes from "./routes";
 
-createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);
+const router = createBrowserRouter(routes);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
