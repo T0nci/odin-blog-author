8d6b39fec50675c16387897e8af331a0e36c4438
T0nci <138928016+T0nci@users.noreply.github.com>
2024-12-13 19:17:55 +0100

    Add and style Footer Component

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import GithubIcon from "../../assets/github.svg";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles["main-footer"]}>
+      <div className={styles.footer + " container"}>
+        <a href="https://github.com/T0nci/" className={styles.link}>
+          <img src={GithubIcon} alt="Tonci's GitHub" />
+          T0nci
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
