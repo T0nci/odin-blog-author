2867666652a3a2f74f848a1adf8fa642513505fc
T0nci <138928016+T0nci@users.noreply.github.com>
2024-12-13 21:02:51 +0100

    Create and style Login Component

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,103 @@
+import { useState } from "react";
+import { Navigate, useNavigate } from "react-router";
+import Footer from "../Footer/Footer";
+import Errors from "../Errors/Errors";
+import styles from "./Login.module.css";
+
+const Login = () => {
+  const [fields, setFields] = useState({ username: "", password: "" });
+  const [errors, setErrors] = useState([]);
+  const navigate = useNavigate();
+
+  if (localStorage.getItem("token")) return <Navigate to="/" />;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const resJson = await fetch(
+        import.meta.env.VITE_API_URL + "/login-author",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify(fields),
+          method: "POST",
+        },
+      );
+
+      const res = await resJson.json();
+
+      if (res.token) {
+        localStorage.setItem("token", res.token);
+        navigate("/");
+      } else if (res.error || res.errors) setErrors([{ msg: "401" }]);
+      else throw new Error("Unknown response");
+    } catch (error) {
+      console.error(error);
+      setErrors([{ msg: "Unknown error." }]);
+    }
+  };
+
+  return (
+    <>
+      <main>
+        <div className="container">
+          <form onSubmit={handleSubmit} className={styles.form}>
+            <h1 className={styles.heading}>Login</h1>
+            {errors.length > 0 && <Errors errors={errors} />}
+            <div>
+              <label htmlFor="username" className={styles.label}>
+                Username:
+              </label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                required
+                autoFocus
+                autoComplete="off"
+                value={fields.username}
+                onChange={(e) =>
+                  setFields((prevFields) => ({
+                    ...prevFields,
+                    username: e.target.value,
+                  }))
+                }
+                className={styles.input}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className={styles.label}>
+                Password:
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                required
+                value={fields.password}
+                onChange={(e) =>
+                  setFields((prevFields) => ({
+                    ...prevFields,
+                    password: e.target.value,
+                  }))
+                }
+                className={styles.input}
+              />
+            </div>
+            <div>
+              <button type="submit" className={styles.submit}>
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Login;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,13 @@
 import App from "./App";
 import ErrorPage from "./Components/ErrorPage/ErrorPage";
+import Login from "./Components/Login/Login";
 
 const routes = [
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <ErrorPage />,
+  },
   {
     path: "/",
     element: <App />,
