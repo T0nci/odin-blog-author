49b4b27fa0754645c367587be8a92c3de6b6163d
T0nci <138928016+T0nci@users.noreply.github.com>
2024-12-20 17:33:17 +0100

    Add formatDate function

--- src/utils.js
+++ src/utils.js
@@ -2,4 +2,14 @@ const formatTitle = (title) => {
   return title.length < 50 ? title : title.slice(0, 47) + "...";
 };
 
-export { formatTitle };
+const formatDate = (date) => {
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = date.getFullYear();
+
+  return `${hours}:${minutes} ${day}.${month}.${year}`;
+};
+
+export { formatTitle, formatDate };
